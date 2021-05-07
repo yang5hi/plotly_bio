2d88fa53a4d071cc5281fc1c706f3275b562b54a
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-05-07 08:49:51 -0400

    file organization

---
+++ static/js/app.js
@@ -0,0 +1,48 @@
+//{"names":["940",...],
+//  "metadata":[{"id":"940",ethnicity, gender, age, location, bbtype, wfreq},...],
+// "samples":[{"id":"940", otu_ids:[], otu_labels:[]},...]}
+
+// Fetch the JSON data and console log it
+d3.json("data/samples.json").then(function(data) {
+    console.log(data);
+  });
+
+// // Initializes the page with a default plot
+// function init() {
+//     data = [{
+//       x: [1, 2, 3, 4, 5],
+//       y: [1, 2, 4, 8, 16] }];
+  
+//     Plotly.newPlot("plot", data);
+//   }
+  
+//   // Call updatePlotly() when a change takes place to the DOM
+//   d3.selectAll("#selDataset").on("change", updatePlotly);
+  
+//   // This function is called when a dropdown menu item is selected
+//   function updatePlotly() {
+//     // Use D3 to select the dropdown menu
+//     var dropdownMenu = d3.select("#selDataset");
+//     // Assign the value of the dropdown menu option to a variable
+//     var dataset = dropdownMenu.property("value");
+  
+//     // Initialize x and y arrays
+//     var x = [];
+//     var y = [];
+  
+//     if (dataset === 'dataset1') {
+//       x = [1, 2, 3, 4, 5];
+//       y = [1, 2, 4, 8, 16];
+//     }
+  
+//     else if (dataset === 'dataset2') {
+//       x = [10, 20, 30, 40, 50];
+//       y = [1, 10, 100, 1000, 10000];
+//     }
+  
+//     // Note the extra brackets around 'x' and 'y'
+//     Plotly.restyle("plot", "x", [x]);
+//     Plotly.restyle("plot", "y", [y]);
+//   }
+  
+//   init();
