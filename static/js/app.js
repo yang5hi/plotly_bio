fe42b86edcaafa00e353dc49befdb39eee79b889
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-05-12 11:27:04 -0400

    bar plot;

--- static/js/app.js
+++ static/js/app.js
@@ -7,34 +7,54 @@ d3.json("data/samples.json").then(function(dataSamples) {
     // log into console to check the array size
     console.log(dataSamples);
     console.log(dataSamples["names"]);
+
+   // make sure every object is unique in the dataset
     let uniqueNames=dataSamples["names"].filter((item, i, ar) => ar.indexOf(item) === i);
-    // make sure every object is unique in the dataset
     console.log(uniqueNames);
+    
     // apppend the subject id into the dropdown menu
     uniqueNames.forEach(name => d3.select("#selDataset").append("option").text(name));
     d3.select("#selDataset").on("change", updatePage);
 
-
+    // function to change page content
     function updatePage() {
-      // get the Test Subject ID no from the dropdown menu
-      let chooseId=d3.select("#selDataset").property("value");
-      console.log(chooseId);
+      //-------------------------update demographic info---------------------------------------
+      // get the Test Subject ID number from the dropdown menu
+      let selectedId=d3.select("#selDataset").property("value");
+      console.log(selectedId);
 
       // match the id with metadata
       d3.select("#sample-metadata").html("");
-      let chooseMeta=dataSamples['metadata'].filter(sampleInfo =>sampleInfo["id"]==chooseId);
-      console.log(chooseMeta[0]);
+      let selectedMeta=dataSamples['metadata'].filter(sampleInfo =>sampleInfo["id"]==selectedId);
+      console.log(selectedMeta[0]);
 
       // output the Demographic Info
-      chooseMeta.forEach(demoInfo=> {
+      selectedMeta.forEach(demoInfo=> {
         Object.entries(demoInfo).forEach(([key,value])=> {
           d3.select("#sample-metadata").append("p").text(`${key}: ${value}`);
         })
       });
 
+      //--------------------------create the horizontal bar chart-------------------------------
       // get the top 10 OTUs
-      let chooseOtus=dataSamples['samples'].filter(otu => otu["id"]==chooseId);
-      console.log(chooseOtus[0]);
+      let selectedOtus=dataSamples['samples'].filter(otu => otu["id"]==selectedId);
+      //console.log(selectedOtus[0].otu_ids);
+      console.log(selectedOtus[0]);
+
+      let otuIds=selectedOtus[0].otu_ids.slice(0,10).map(id=>`OTU ${id}`).reverse();
+      let sampleValues=selectedOtus[0].sample_values.slice(0,10).reverse();
+      let otuLabels=selectedOtus[0].otu_labels.slice(0,10).reverse();
+      console.log(otuIds);
+      var data = [{
+        type: 'bar',
+        x: sampleValues,
+        y: otuIds,
+        text: otuLabels,
+        orientation: 'h'
+      }];
+      
+      Plotly.newPlot('bar', data);
+
     };
     
   
@@ -82,3 +102,14 @@ d3.json("data/samples.json").then(function(dataSamples) {
 //   }
   
 //   init();
+
+/* horizontal bar chart
+var data = [{
+  type: 'bar',
+  x: [20, 14, 23],
+  y: ['giraffes', 'orangutans', 'monkeys'],
+  orientation: 'h'
+}];
+
+Plotly.newPlot('myDiv', data);
+*/
