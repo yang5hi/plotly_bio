a25ecdb44846c44094ee147d797e1b43673387e5
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-05-12 13:54:16 -0400

    function init()

--- static/js/app.js
+++ static/js/app.js
@@ -1,89 +1,69 @@
-
-// Fetch the JSON data and console log it
-d3.json("data/samples.json").then(function(dataSamples) {
-    // log into console to check the array size
-    console.log(dataSamples);
-    console.log(dataSamples["names"]);
-
-   // make sure every object is unique in the dataset
-    let uniqueNames=dataSamples["names"].filter((item, i, ar) => ar.indexOf(item) === i);
-    console.log(uniqueNames);
-    
-    // apppend the subject id into the dropdown menu
-    uniqueNames.forEach(name => d3.select("#selDataset").append("option").text(name));
-    d3.select("#selDataset").on("change", updateDemographicInfo);
-
-    // function to change page content
-    function updateDemographicInfo() {
+// Initialize the page
+function init() {updatePage();};
+// Function update the page content
+function updatePage() {
+  d3.json("data/samples.json").then(function(dataSamples) {
+      // Fetch the JSON data and log it into console to check the array size
+      console.log(dataSamples);
+      // make sure every object is unique in the dataset
+      let uniqueNames=dataSamples["names"].filter((item, i, ar) => ar.indexOf(item) === i);
+      console.log(uniqueNames);
+      // apppend the subject id into the dropdown menu
+      uniqueNames.forEach(name => d3.select("#selDataset").append("option").text(name));
       //-------------------------update demographic info---------------------------------------
       // get the Test Subject ID number from the dropdown menu
       let selectedId=d3.select("#selDataset").property("value");
-      console.log(selectedId);
+      console.log(selectedId); // validate ID
+      d3.select("#sample-metadata").html(""); // clear the previrous paragraphs
       // match the id with metadata
-      d3.select("#sample-metadata").html("");
       let selectedMeta=dataSamples['metadata'].filter(sampleInfo =>sampleInfo["id"]==selectedId);
-      console.log(selectedMeta[0]);
-
-      // output the Demographic Info
+      console.log(selectedMeta[0]); // validate the selected metadata
+      // output the Demographic Info to DOM
       selectedMeta.forEach(demoInfo=> {
-        Object.entries(demoInfo).forEach(([key,value])=> {
-          d3.select("#sample-metadata").append("p").text(`${key}: ${value}`);
-        })
+          Object.entries(demoInfo).forEach(([key,value])=> {
+              d3.select("#sample-metadata").append("p").text(`${key}: ${value}`);
+            })
       });
       //--------------------------create the horizontal bar chart-------------------------------
-      // get the top 10 OTUs
+      // select the sample otu data based on id
       let selectedOtus=dataSamples['samples'].filter(otu => otu["id"]==selectedId);
-      console.log(selectedOtus[0]);
-
-      // .reverse();
+      // store id, value, and labels to an array
       let otuIds=selectedOtus[0].otu_ids;
       let sampleValues=selectedOtus[0].sample_values;
       let otuLabels=selectedOtus[0].otu_labels;
       let selectedList=otuIds.map((a,i)=>[a,sampleValues[i],otuLabels[i]]);
       console.log(selectedList);
+      // get the top 10 OTUs 
       sortedList=selectedList.sort((a,b)=>(b[1]-a[1])).slice(0,10);
-      console.log(sortedList);
+      console.log(sortedList); // validate the data
+      // bar graph
       var data = [{
-        type: 'bar',
-        x: sortedList.map(a=>a[1]).reverse(),
-        y: sortedList.map(a=>`OTU ${a[0]}`).reverse(),
-        text: sortedList.map(a=>a[2]).reverse(),
-        orientation: 'h'
+          type: 'bar',
+          x: sortedList.map(a=>a[1]).reverse(),
+          y: sortedList.map(a=>`OTU ${a[0]}`).reverse(),
+          text: sortedList.map(a=>a[2]).reverse(),
+          orientation: 'h'
       }];
-      
       Plotly.newPlot('bar', data);
-     
-
-      //------------------------------create bubble chart-----------------------------------------------
-     var trace1 = [{
+    //------------------------------create bubble chart-----------------------------------------------
+      var trace1 = [{
           x: otuIds,
           y: sampleValues,
           text: selectedList,
           mode: 'markers',
           marker: {
-            color: otuIds,
-            size: sampleValues
+              color: otuIds,
+              size: sampleValues
           }
         }];
-
       var layout = {
-        xaxis: {title: 'OTU ID'},
-        showlegend: false
-      };
-
+          xaxis: {title: 'OTU ID'},
+          showlegend: false
+        };
       Plotly.newPlot('bubble', trace1, layout);
-    };  
-    
-    
   });
+};
 
+d3.select("#selDataset").on("change", updatePage);
 
-
-// // Initializes the page with a default plot
-// function init() {
-//     data = [{
-//       x: [1, 2, 3, 4, 5],
-//       y: [1, 2, 4, 8, 16] }];
-  
-//     Plotly.newPlot("plot", data);
-//   }
+init();
