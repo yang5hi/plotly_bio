d68377b88f7c68ca658fbe9acb1acb809c8c253c
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-05-13 14:50:04 -0400

    remove init()

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,8 @@
 // Initialize the page
-function init() {updatePage();};
+UpdatePage();
+
 // Function update the page content
-function updatePage() {
+function UpdatePage() {
   d3.json("data/samples.json").then(function(dataSamples) {
       // Fetch the JSON data and log it into console to check the array size
       console.log(dataSamples);
@@ -22,7 +23,7 @@ function updatePage() {
       selectedMeta.forEach(demoInfo=> {
           Object.entries(demoInfo).forEach(([key,value])=> {
               d3.select("#sample-metadata").append("p").append("strong").text(`${key}: ${value}`);
-            })
+          })
       });
       // select the sample otu data based on id
       let selectedOtus=dataSamples['samples'].filter(otu => otu["id"]==selectedId);
@@ -37,84 +38,80 @@ function updatePage() {
       console.log(sortedList); // validate the data
       //create the horizontal bar chart
       PlotBar(sortedList, selectedId);
-      //----create bubble chart---------------------
-      PlotBubble (otuIds, sampleValues,selectedList, selectedId);
-      
-      //--------------------------create the guage chart-------------------------------Bonus Part
-      var trace2={
-            value: selectedMeta[0].wfreq,
-            domain: { x: [0, 1], y: [0, 1] },
-            title: { text: "Scrubs per Week" },
-            type: "indicator",
-            margin: { t: 0, b: 0 },
-            gauge: {
-                axis: { range: [null, 9] },
-                bar: { color: "darkblue" },
-                steps: [{ range: [0,1], color: '#ffffff'},
-                        { range: [1,2], color: '#f3cec9'},
-                        { range: [2,3], color: '#e7a4b6'},
-                        { range: [3,4], color: '#cd7eaf'},
-                        { range: [4,5], color: '#a262a9'},
-                        { range: [5,6], color: '#6f4d96'},
-                        { range: [6,7], color: '#3d3b72'},
-                        { range: [7,8], color: '#182844'},
-                        { range: [8,9], color: '#000000'}
-                ]},
-            mode: "gauge+number"
-        };      
-        var layout = {
-            title: 'Belly Button Washing Frequency',
-            font: {size: 20}
-        };
-      Plotly.newPlot('gauge', [trace2] , layout);
+      //create bubble chart
+      PlotBubble (otuIds, sampleValues,selectedList, selectedId);  
+      //create the gauge chart(Bonus Part)
+      PlotGauge (selectedMeta[0].wfreq);
   });
 };
 
 function PlotBar (sortedList, selectedId) {
-    // bar graph
     var data = [{
-        type: 'bar',
-        x: sortedList.map(a=>a[1]).reverse(),
-        y: sortedList.map(a=>`OTU${a[0]} `).reverse(),
-        text: sortedList.map(a=>a[2]).reverse(),
-        marker: {
-          color: '#C8A2C8',
-          line: {
-              width: 2.5
-          }
-      },
-        orientation: 'h'
-    }];
+            type: 'bar',
+            x: sortedList.map(a=>a[1]).reverse(),
+            y: sortedList.map(a=>`OTU${a[0]} `).reverse(),
+            text: sortedList.map(a=>a[2]).reverse(),
+            marker: {
+                color: '#C8A2C8',
+                line: {width: 2.5}
+            },
+            orientation: 'h'
+        }];
     var layout = {
-      title: `top 10 OTUs found in test subject ${selectedId}`,
-      xaxis: {title: 'OTU Values'},
-      font: {size: 14},
-      showlegend: false
-    };
+            title: `top 10 OTUs in test subject ${selectedId}`,
+            xaxis: {title: 'OTU Values'},
+            font: {size: 14},
+            showlegend: false
+        };
     Plotly.newPlot('bar', data, layout);
 };
 
 function PlotBubble(otuIds, sampleValues,selectedList, selectedId){
-  //bubble graph
   var trace1 = {
-      x: otuIds,
-      y: sampleValues,
-      text: selectedList,
-      mode: 'markers',
-      marker: {
-          color: otuIds,
-          size: sampleValues
-      }
-    };
+          x: otuIds,
+          y: sampleValues,
+          text: selectedList,
+          mode: 'markers',
+          marker: {
+              color: otuIds,
+              size: sampleValues
+          }
+      };
   var layout = {
-      title: `all OTUs found in test subject ${selectedId}`,
-      font: {size: 14},
-      xaxis: {title: 'OTU ID Number'},
-      yaxis: {title: 'OTU Values'},
-      showlegend: false
-    };
+          title: `all OTUs found in test subject ${selectedId}`,
+          font: {size: 14},
+          xaxis: {title: 'OTU ID Number'},
+          yaxis: {title: 'OTU Values'},
+          showlegend: false
+      };
   Plotly.newPlot('bubble', [trace1], layout);
 };
-d3.select("#selDataset").on("change", updatePage);
 
-init();
+function PlotGauge (inNum) {
+  var trace2={
+        value: inNum,
+        title: { text: "Scrubs per Week" },
+        type: "indicator",
+        gauge: {
+            axis: { range: [null, 9] },
+            bar: { color: "darkblue" },
+            steps: [{ range: [0,1], color: '#ffffff'},
+                    { range: [1,2], color: '#f3cec9'},
+                    { range: [2,3], color: '#e7a4b6'},
+                    { range: [3,4], color: '#cd7eaf'},
+                    { range: [4,5], color: '#a262a9'},
+                    { range: [5,6], color: '#6f4d96'},
+                    { range: [6,7], color: '#3d3b72'},
+                    { range: [7,8], color: '#182844'},
+                    { range: [8,9], color: '#000000'}]
+        },
+        mode: "gauge+number"
+    };      
+    var layout = {
+            title: 'Belly Button Washing Frequency',
+            font: {size: 20}
+        };
+  Plotly.newPlot('gauge', [trace2] , layout);
+};
+
+d3.select("#selDataset").on("change", UpdatePage);
